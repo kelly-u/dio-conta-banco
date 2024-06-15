f63a28166a521b197f0ce553955a5861d05c4b13
G. Kelly Macedo <gleicekellymacedodd@gmail.com>
2024-06-15 01:30:49 -0300

    Ajustei a classe ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,14 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
-    int numero = 1021;
-    String agencia = "067-8";
-    String nomeCliente = "MARIO ANDRADE";
-    Double saldo = 237.48;
-
     public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
         System.out.println("Digite o número da agência:");
+        int numero = scanner.nextInt();
+
         System.out.println("Digite a agência:");
+        String agencia = scanner.next();
+
         System.out.println("Digite o nome do cliente:");
+        String cliente = scanner.next();
+
         System.out.println("Digite o valor do saldo:");
+        double saldo = scanner.nextDouble();
+
+
+        System.out.println("Olá " + cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+
     }
 }
