e7c6a5028f9a53a0dfd382a6e17b5079438f8404
G. Kelly Macedo <gleicekellymacedodd@gmail.com>
2024-06-15 01:12:21 -0300

    Iniciando o projeto

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,14 @@
+public class ContaTerminal {
+
+    int numero = 1021;
+    String agencia = "067-8";
+    String nomeCliente = "MARIO ANDRADE";
+    Double saldo = 237.48;
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Digite o número da agência:");
+        System.out.println("Digite a agência:");
+        System.out.println("Digite o nome do cliente:");
+        System.out.println("Digite o valor do saldo:");
+    }
+}
